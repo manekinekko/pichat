607da385dea26e2ebc700593e1f6473d7e66b7fa
Wassim CHEGHAM <github@wassimchegham.com>
2016-12-02 13:15:35 +0100

    feat(firebase): add firebase configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,18 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDElp_wKrZBWgh-rhsfRlsyHKm-101Lqr0",
+  authDomain: "pichat-381aa.firebaseapp.com",
+  databaseURL: "https://pichat-381aa.firebaseio.com",
+  storageBucket: "pichat-381aa.appspot.com",
+  messagingSenderId: "689229560693"
+};
 
 @NgModule({
   declarations: [
